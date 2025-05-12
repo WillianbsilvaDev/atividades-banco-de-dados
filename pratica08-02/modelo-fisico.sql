72641cc81ec4058b4df585c5cf687553b14d6542
WillianbsilvaDev <willian.dsilva@sptech.school>
2025-05-12 20:37:59 -0300

    nova pratica(pratica -8-2) não finalizada

---
+++ pratica08-02/modelo-fisico.sql
@@ -0,0 +1,31 @@
+
+-- Após a modelagem, implementar no MySQL:
+1. Criar as tabelas no MySQL e executar as instruções relacionadas a seguir.
+2. Insira dados nas tabelas, de forma que tenha pelo menos 2 grupos
+cadastrados.
+Após inserir dados nas tabelas:
+3. Exibir todos os dados de cada tabela criada, separadamente.
+4. Se for necessário, fazer os acertos da(s) chave(s) estrangeira(s).
+5. Exibir os dados dos grupos e os dados de seus respectivos alunos.
+6. Exibir os dados de um determinado grupo e os dados de seus respectivos
+alunos.
+7. Exibir a média das notas atribuídas aos grupos, no geral.
+8. Exibir a nota mínima e a nota máxima que foi atribuída aos grupos, no geral.
+9. Exibir a soma das notas dadas aos grupos, no gera
+10. Exibir os dados dos professores que avaliaram cada grupo, os dados dos
+grupos, a data e o horário da avaliação.
+11. Exibir os dados dos professores que avaliaram um determinado grupo, os
+dados do grupo, a data e o horário da avaliação.
+12. Exibir os nomes dos grupos que foram avaliados por um determinado
+professor.
+13. Exibir os dados dos grupos, os dados dos alunos correspondentes, os
+dados dos professores que avaliaram, a data e o horário da avaliação.
+14. Exibir a quantidade de notas distintas.
+15. Exibir a identificação do professor (pode ser o fk ou o nome), a média das
+notas e a soma das notas atribuídas, agrupadas por professor.
+16. Exibir a identificação do grupo (pode ser o fk ou o nome), a média das notas
+e a soma das notas atribuídas, agrupadas por grupo.
+17. Exibir a identificação do professor (pode ser o fk ou o nome), a menor nota
+e a maior nota atribuída, agrupada por professor.
+18. Exibir a identificação do grupo (pode ser o fk ou o nome), a menor nota e a
+maior nota atribuída, agrupada por grupo.
